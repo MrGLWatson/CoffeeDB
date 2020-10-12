cabfeb75af4560e28d370012de6d99ddbf3f5094
Gary Watson <Gary_Watson@Blueyonder.co.uk>
2020-10-12 11:43:49 +0100

    Initial Commit

---
+++ CoffeeDB/CoffeeDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeDBApp.swift
+//  CoffeeDB
+//
+//  Created by Gary Watson on 12/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
